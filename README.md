af70f2c7c12fbb70ace4e2705d3dcdabcf033643
Valentina <145690430+hdvalen@users.noreply.github.com>
2025-02-28 11:17:14 -0500

    feat: :fire: sql&DescripciondeDiseño

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
     Base de datos para un concesionario de vehículos que gestione la información sobre los vehículos en stock, clientes, ventas, y servicios de mantenimiento. La base de datos debe permitir registrar y gestionar vehículos, vendedores, clientes, transacciones de ventas y servicios realizados. Los estudiantes deben crear un diagrama UML E-R que represente la estructura de la base de datos y entregar una documentación detallada que explique las decisiones de diseño, las relaciones entre las tablas y las restricciones impuestas.
     
 ## Entidades y atributos
-    
-## Vehiculos 
+
+### Concesionario
+    NIT
+    nombre
+    direccion
+
+### Vehiculos 
     Marca
     Modelo
     Año
@@ -13,33 +18,59 @@
     Tipo Combustible
     Tipo transmisión
     Estado(nuevo, usado)
+    idConcesionario
     Debe tener disponibilidad actualizada en el inventario
 
-## Clientes
+### Cliente
     Nombre
     Teléfono
     Correo Electrónico
     Direccion
+    idVenta
     
-## Vendedores
+### Vendedores
     Nombre
     Numero Empleado
     Fecha Contratacion
-   
+    idConcesionario
 
-## Ventas
+### Ventas
     Fecha Venta
     Total Transaccion
     Metodo Pago(Tarjeta, Tranferencia)
+    idVehiculos
 
-## Mantenimiento
+### Mantenimiento
     TipoServicio(Preventivo, correctivo)
     Costo
     FechaServicio
+    idDetallerMovimiento
+    idVehiculos
     Los vehiculos pueden recibir mantenimiento
 
-## Relaciones
-    Un clientes puede comprar varios vehiculos y solicitar varios servicios de mantenimiento
-    El vendedor puede realizar varias ventas
-    El vendedor se asocia a varias transacciones de venta
+### Detaller Movimiento
+    idDetallerMovimiento
+    idServicios
+
+### Servicios
+    Descrpcion
+    idDetallerMovimiento
+
+## Justificacion de Diseño 
+
+    -Concesionario - Vehículos: Un concesionario tiene muchos vehículos (relación de 1 a N).
+    -Concesionario - Vendedores: Un concesionario tiene varios vendedores (relación de 1 a N).
+    -Vehículos - Mantenimiento: Un vehículo puede tener múltiples mantenimientos (relación de 1 a N).
+    -Ventas - Vehículos: Un vehículo puede estar involucrado en varias ventas, pero cada -venta se refiere a un solo vehículo (relación de N a 1).
+    -Ventas - Clientes: Un cliente puede realizar muchas compras, pero cada venta es realizada a un solo cliente (relación de N a 1).
+    -Ventas - Vendedores: Un vendedor puede realizar muchas ventas (relación de N a 1).
+    -Mantenimiento - DetalleMovimiento: Un mantenimiento está asociado a un detalle de movimiento específico (relación de N a 1).
+    -DetalleMovimiento - Servicios: Un detalle de movimiento puede tener muchos servicios relacionados (relación de N a 1).
+    -Se deb mantener la disponibilidad actualizada de los vehiculos en un inventario
+
+## Diagrama UML E-R
+
+    ![alt text](images/image.png)
+
+
 
--- index.sql
+++ index.sql
@@ -1,59 +1,72 @@
 CREATE DATABASE campusCar;
 
-CREATE TABLE vehiculos(
-    idVehiculos INT PRIMARY KEY 
-    marca
-    modelo
-    año
-    numeroSerie
-    precio
-    color
-    tipoCombustible
-    tipoTransmision
-    estado
-);
-
-CREATE TABLE clientes(
-    idClientes INT PRIMARY KEY
-    nombre
-    telefono
-    correoElectronico
-    direccion
-);
-
-CREATE TABLE vendedores(
-    idVendedores INT PRIMARY KEY 
-    nombre
-    numeroEmpleado
-    fechaContratacion
-);
-
-CREATE TABLE ventas(
-    idVentas INT PRIMARY KEY 
-    fechaVenta
-    totalTransaccion
-    metodoPago
-);
-
-CREATE TABLE mantenimieto(
-    idMantenimiento INT PRIMARY KEY 
-    tipoServicio
-    costo
-    fechaServicio
-)
-
 CREATE TABLE concesionario (
-    idConcesionario INT PRIMARY KEY 
-)
+    idConcesionario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    direccion VARCHAR(50),
+    telefono NUMERIC(10),
+    correoElectronico VARCHAR(50)
+);
 
-CREATE TABLE detalleMovimiento(
-    idDetalleMovimiento INT PRIMARY KEY 
-)
+CREATE TABLE vehiculos (
+    idVehiculos INT AUTO_INCREMENT PRIMARY KEY, 
+    marca VARCHAR(50),
+    modelo VARCHAR(50),
+    año DATE,
+    numeroSerie VARCHAR(50),
+    precio NUMERIC,
+    color VARCHAR(50),
+    tipoCombustible VARCHAR(50),
+    tipoTransmision VARCHAR(50),
+    estado VARCHAR(50),
+    VIN NUMERIC,
+    idConcesionario INT,
+    CONSTRAINT fk_concesionario FOREIGN KEY (idConcesionario) REFERENCES concesionario(idConcesionario)
+);
 
-CREATE TABLE inventario(
-    idInventario INT PRIMARY KEY 
-)
+CREATE TABLE clientes (
+    idCliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    telefono NUMERIC(10),
+    correoElectronico VARCHAR(50),
+    direccion VARCHAR(50)
+);
 
-CREATE TABLE detalleVenta(
-    idDetalleVenta INT PRIMARY KEY 
-)
+CREATE TABLE vendedores (
+    idVendedores INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    numeroEmpleado NUMERIC,
+    fechaContratacion DATE,
+    idConcesionario INT,
+    CONSTRAINT fk_concesionario FOREIGN KEY (idConcesionario) REFERENCES concesionario(idConcesionario)
+);
+
+CREATE TABLE ventas (
+    idVentas INT AUTO_INCREMENT PRIMARY KEY, 
+    fechaVenta DATE,
+    totalTransaccion NUMERIC,
+    metodoPago VARCHAR(50),
+    idVendedores INT,
+    idCliente INT,
+    CONSTRAINT fk_vendedores FOREIGN KEY (idVendedores) REFERENCES vendedores(idVendedores),
+    CONSTRAINT fk_clientes FOREIGN KEY (idCliente) REFERENCES clientes(idCliente)
+);
+
+CREATE TABLE mantenimiento (
+    idMantenimiento INT AUTO_INCREMENT PRIMARY KEY, 
+    costo NUMERIC,
+    fechaServicio DATE,
+    idVehiculos INT,
+    CONSTRAINT fk_vehiculos FOREIGN KEY (idVehiculos) REFERENCES vehiculos(idVehiculos)
+);
+
+CREATE TABLE detalleMovimiento (
+    idDetalleMovimiento INT AUTO_INCREMENT PRIMARY KEY,
+    idServicios INT,
+    CONSTRAINT fk_servicios FOREIGN KEY (idServicios) REFERENCES servicios(idServicios)
+);
+
+CREATE TABLE servicios (
+    idServicios INT AUTO_INCREMENT PRIMARY KEY, 
+    descripcion VARCHAR(50)
+);
